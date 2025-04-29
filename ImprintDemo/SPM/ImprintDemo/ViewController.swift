603cb83b64bc29e81746559ae927d24e2d36c1ad
Xingtan0717 <96141130+Xingtan0717@users.noreply.github.com>
2025-04-29 15:43:01 -0700

    (FEAT)[SPND-1182] Update CI for validating Swift Package Manager Integration (#17)
    * (FEAT)[SPND-1182] Update CI for validating Swift Package Manager Integration
    
    * update the ci yml
    
    * remove swift package init
    
    * update ci.yml
    
    * fix dependencies
    
    * fix package name
    
    * update the swiftbuild target
    
    * re-architecture the demo app folder
    
    * fix typos
    
    * patch SPM script
    
    * update ci.yml
    
    * fix the patching method
    
    * Try different approach

---
+++ ImprintDemo/SPM/ImprintDemo/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Wanting Shao on 10/7/24.
+//
+
+import UIKit
+import Imprint
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var clientSecretInput: UITextView!
+  @IBOutlet weak var partnerRefInput: UITextView!
+  @IBOutlet weak var environmentSelector: UISegmentedControl!
+  @IBOutlet weak var completionState: UITextView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupLayout()
+    environmentSelector.selectedSegmentIndex = 1
+    
+    // Prefill your token if needed
+    clientSecretInput.text = ""
+  }
+
+  @IBAction func startTapped(_ sender: Any) {
+    let clientSecret = clientSecretInput.text ?? ""
+    let partnerReference = partnerRefInput.text ?? ""
+    let environment = ImprintConfiguration.Environment(rawValue: environmentSelector.selectedSegmentIndex) ?? .staging
+    
+    let configuration = ImprintConfiguration(clientSecret: clientSecret, partnerReference: partnerReference, environment: environment)
+    
+    configuration.onCompletion = { state, data in
+      switch state {
+      case .offerAccepted:
+        self.completionState.text = "Offer accepted\n\(self.jsonString(data))"
+      case .rejected:
+        self.completionState.text = "Application rejected\n\(self.jsonString(data))"
+      case .inProgress:
+        self.completionState.text = "Application Interrupted - In Progress"
+      case .error:
+        self.completionState.text = "Error occured\n\(self.jsonString(data))"
+      @unknown default:
+        break
+      }
+    }
+    
+    ImprintApp.startApplication(from: self, configuration: configuration)
+  }
+  
+  private func setupLayout(){
+    clientSecretInput.layer.borderWidth = 1
+    clientSecretInput.layer.borderColor = UIColor.secondaryLabel.cgColor
+    clientSecretInput.layer.cornerRadius = 6
+    
+    partnerRefInput.layer.borderWidth = 1
+    partnerRefInput.layer.borderColor = UIColor.secondaryLabel.cgColor
+    partnerRefInput.layer.cornerRadius = 6
+    
+    completionState.layer.borderWidth = 1
+    completionState.layer.borderColor = UIColor.secondaryLabel.cgColor
+    completionState.layer.cornerRadius = 6    
+  }
+  
+  // Helper
+  private func jsonString(_ dictionary: [String: Any]?) -> String {
+    guard let dictionary else { return "nil" }
+    if let data = try? JSONSerialization.data(withJSONObject: dictionary, options: .prettyPrinted),
+       let jsonString = String(data: data, encoding: .utf8) {
+      return jsonString
+    } else {
+      return (String(describing: dictionary))
+    }
+  }
+}
+
